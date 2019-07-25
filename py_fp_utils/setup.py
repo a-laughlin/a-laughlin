bfdb2ce6642012a175bd2d66ddbf7fd6f7df7e73
Adam Laughlin <adam.laughlin@gmail.com>
2019-07-25 16:48:33 -0600

    update py setup version

--- py_fp_utils/setup.py
+++ py_fp_utils/setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup,find_packages
 setup(name='fp_utils',
-      version='0.0.2',
+      version='0.0.3',
       description='This package has shared components.',
       author='Adam Laughlin',
       author_email='',
