dc2cff5d777373c41e8c9166d9c87ec2b736ff4c
Adam Laughlin <adam.laughlin@gmail.com>
2019-07-25 16:47:53 -0600

    change directories to see if that works

---
+++ py_fp_utils/fp_utils/__init__.py
@@ -0,0 +1,1 @@
+name = "fp_utils"
--- py_fp_utils/setup.py
+++ py_fp_utils/setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 from setuptools import setup,find_packages
-setup(name='a_laughlin_utils',
+setup(name='fp_utils',
       version='0.0.2',
       description='This package has shared components.',
       author='Adam Laughlin',
       author_email='',
-      url='https://github.com/a-laughlin/a-laughlin.git',
-      packages=find_packages(where="py"),#,exclude=['*test.py']
+      url='https://github.com/a-laughlin/a-laughlin',
+      packages=find_packages(),
       license='LICENSE.txt',
     )
