bb3f1c830188f6202afd3c82483b95071ccf28ba
Adam Laughlin <adam.laughlin@gmail.com>
2019-07-25 15:29:59 -0600

    update pypackage naming and locations

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@
 
 from setuptools import setup,find_packages
 
-setup(name='a-laughlin-utils',
-      version='0.0.1',
+setup(name='a_laughlin_utils',
+      version='0.0.2',
       description='This package has shared components.',
       author='Adam Laughlin',
       author_email='',
       url='https://github.com/a-laughlin/a-laughlin.git',
-      packages=['py/fp_utils','py/jupyter_utils','py/pandas_utils','py/vis_utils'],
+      packages=find_packages(where="./py/",include=[
+        'fp_utils','jupyter_utils','pandas_utils','vis_utils'
+      ]),
       license='LICENSE.txt',
     )
