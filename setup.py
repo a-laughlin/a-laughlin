8ef392321c582c0bd66a68d144e4b0f6aea1e8f3
Adam Laughlin <adam.laughlin@gmail.com>
2019-07-25 16:04:30 -0600

    try py install again

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 
 from setuptools import setup,find_packages
-
 setup(name='a_laughlin_utils',
       version='0.0.2',
       description='This package has shared components.',
       author='Adam Laughlin',
       author_email='',
       url='https://github.com/a-laughlin/a-laughlin.git',
-      packages=find_packages(where="py",exclude=['*test.py']),
+      packages=find_packages(where="py"),#,exclude=['*test.py']
       license='LICENSE.txt',
     )
