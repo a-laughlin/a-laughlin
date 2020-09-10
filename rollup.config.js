78a510957f063361a1e2e02a61c1aede0a9f548f
alaughlin <alaughlin@switchfly.com>
2020-09-10 00:21:02 -0600

    lots of build updates to hopefully get imports working on all platforms

--- rollup.config.js
+++ rollup.config.js
@@ -2,22 +2,27 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs'; // only for xstream and react
 import visualizer from 'rollup-plugin-visualizer';
-// import {default as transpile} from '@rollup/plugin-sucrase';
 import {terser} from 'rollup-plugin-terser';
 import copy from 'rollup-plugin-cpy';
 import alias from '@rollup/plugin-alias';
 import analyze from 'rollup-plugin-analyzer';
-// const prettier = require('rollup-plugin-prettier');
-// const eslint = require('rollup-plugin-prettier');
 import sourcemap from 'rollup-plugin-sourcemaps';
 
-const snakeToStartCase = s=>s.split('-').map(s=>s[0].toUpperCase()+s.slice(1)).join('');
-import {readdirSync,writeFileSync} from 'fs';
+import {readdirSync,writeFileSync, mkdirSync} from 'fs';
 import {join} from 'path';
-// import terser from '@rollup/plugin-terser';
 import injectProcessEnv from 'rollup-plugin-inject-process-env';
 
-// how to use this for 3 loops, one for dev, one for prod, one for prod-min
+const snakeToStartCase = s=>s.split('-').map((s,i)=>i===0?s:s[0].toUpperCase()+s.slice(1)).join('');
+// import {default as transpile} from '@rollup/plugin-sucrase';
+// const prettier = require('rollup-plugin-prettier');
+// const eslint = require('rollup-plugin-prettier');
+
+// TODO
+// - imports work with directly importing packages/<name>/dist/es/<name>.js, but not by importing 
+//   packages/<name>/dist as specified by module field in package.json.  Why?
+// - tree shaking doesn't work with makeCollectionFn in fp-utils, resulting in many unnecessry exports
+// - how to use processEnvPlugin in the loop for dev and production(min, with srcmaps)
+
 // sets the process object in each environment
 const processEnvPlugin = injectProcessEnv({ 
   NODE_ENV: process.env.NODE_ENV||'production'
@@ -34,6 +39,32 @@ const sourceMapPlugin = sourcemap();
 const terserPlugin = terser();
 
 
+// const getExamplePlugin = ()=>({
+//   name: 'json-packages', // this name will show up in warnings and errors
+//   // returning source avoids rollup looking for the id in other plugins or the file system
+//   resolveId:source =>source === '\0custom-packages' ? source : null,
+//   // loads the source code for virtual module, else passes on to next plugin
+//   load:id=>id === '\0custom-packages' ? 'export default "This is virtual!"' : null,
+//   transform:({format})=>{}
+// });
+// { // example plugin
+//   input: '\0virtual-module',
+//   plugins: [getExamplePlugin()],
+//   output: [{
+//     file: 'dist/virtual-module.js',
+//     format: 'es'
+//   }]
+// }
+
+// package.json in each file based on https://2ality.com/2019/10/hybrid-npm-packages.html#option-3%3A-bare-import-esm%2C-deep-import-commonjs-with-backward-compatibility
+// "author": "Adam Laughlin <adam.laughlin@gmail.com>",
+// "license": "MIT",
+// "main": "cjs",
+// "browser": "umd",
+// "module": "es",
+// "publishConfig": {
+//   "access": "public"
+// },
 const modules = readdirSync('packages')
 .filter(dir=>dir!=='.DS_Store')
 .map(dir=>({
@@ -42,27 +73,55 @@ const modules = readdirSync('packages')
   inDir:join(`packages`,dir,'src'),
   entryFileNames:join(`packages`,dir,'src',`${dir}.js`),
 }))
-.map(({dir,outDir,inDir,entryFileNames})=>({
+.map(({dir,outDir,inDir,entryFileNames,pkg})=>({
   external:[
     'lodash-es',
     'react',
     'react-dom',
     'xstream',
+    // 'graphql', // prevents adding it to bundles
+    // 'graphql-tag'
   ],
   input:join(inDir,`${dir}.js`),
-  output:['es','umd','cjs'].flatMap(format=>[
-    {format, file:join(outDir,format,`${dir}.js`),compact:true, entryFileNames},
-    {format, file:join(outDir,format,`${dir}.min.js`),compact:true,sourcemap:true, entryFileNames, plugins:[terserPlugin]},
-  ]).map(o=>o.format!=='umd'?o:{
-    ...o,
-    name:snakeToStartCase(dir),
-    globals:{
-      react:'react',
-      xstream:'xstream',
-      '@a-laughlin/fp-utils':'fpUtils',
-      '@a-laughlin/style-string-to-object':'styleStringToObject'
-    }
-  }),
+  output:[
+      {format:'es',type:'module'},
+      {format:'umd',type:'umd'},
+      {format:'cjs',type:'commonjs'},
+    ].flatMap(({format,type})=>{
+      const dev = {
+        format,
+        entryFileNames,
+        compact:true,file:join(outDir,format,`${dir}.js`),
+        // based on https://lihautan.com/12-line-rollup-plugin/
+        plugins:[{name:'write-package.json', generateBundle(){
+          mkdirSync(join(outDir,format),{recursive: true});
+          writeFileSync(join(outDir,format,'package.json'),JSON.stringify({type},null,2));
+        }}]
+      };
+      
+      const prod = {
+        ...dev,
+        file:join(outDir,format,`${dir}.min.js`),
+        sourcemap: true,
+        plugins:[terserPlugin]
+      };
+      
+      if(format==='umd'){
+        const globalArgs={
+          name:snakeToStartCase(dir),
+          globals:{
+            react:'react',
+            xstream:'xstream',
+            '@a-laughlin/fp-utils':'fpUtils',
+            '@a-laughlin/style-string-to-object':'styleStringToObject',
+            'graphql-tag':'gql',
+          }
+        };
+        Object.assign(dev,globalArgs);
+        Object.assign(prod,globalArgs);
+      }
+      return [dev, prod];
+    }),
   plugins:[
     alias({entries: [
       // rollup is unaware of yarn workspaces, so alias input paths when building
@@ -74,31 +133,38 @@ const modules = readdirSync('packages')
     commonjsPlugin, // must go after resolve
     processEnvPlugin, // must go after commonjs
     sourceMapPlugin,
-    copy([ { files: join(`packages`,dir,'{LICENSE,package.json}'), dest: outDir }]),
+    copy([ { files: join(`packages`,dir,'{LICENSE}'), dest: outDir }]),
     analyze({
       showExports:true,
       writeTo:s=>writeFileSync(join(`packages`,dir,'generated_stats.txt'),s),
       onAnalysis:o=>writeFileSync(join(`packages`,dir,'generated_stats.json'),JSON.stringify(o,null,2)),
     }),
-    visualizer({ template:"treemap", filename:join(outDir,'stats-treemap.html')}),
-    visualizer({ template:"network", filename:join(outDir,'stats-network.html')}),
+    visualizer({ template:"treemap", filename:join(`packages`,dir,'stats-treemap.html')}),
+    visualizer({ template:"network", filename:join(`packages`,dir,'stats-network.html')}),
+    {name:'write-root-package.json', generateBundle(){
+      const pkg = {
+        "author": "Adam Laughlin <adam.laughlin@gmail.com>",
+        "license": "MIT",
+        "publishConfig": {
+          "access": "public"
+        },
+        ...require(`./packages/${dir}/package.json`),
+      };
+      // this is kind of weird
+      writeFileSync(join(outDir,'package.json'),JSON.stringify({
+        "main": `cjs/${dir}.js`,
+        "browser": `umd/${dir}.js`,
+        "module":`"es/${dir}.js`,
+        ...pkg,
+      }));
+      writeFileSync(join(inDir,'package.json'),JSON.stringify({
+        "main": `${dir}.js`,
+        "type":"module",
+        ...pkg,
+      },null,2));
+    }}
   ],
 }))
 export default modules;
 
-  // const getExamplePlugin = ()=>({
-  //   name: 'my-example', // this name will show up in warnings and errors
-  //   // returning source avoids rollup looking for the id in other plugins or the file system
-  //   resolveId:source =>source === '\0virtual-module' ? source : null,
-  //   // loads the source code for virtual module, else passes on to next plugin
-  //   load:id=>id === '\0virtual-module' ? 'export default "This is virtual!"' : null,
-  //   // transform:()=>{}
-  // });
-  // { // example plugin
-  //   input: '\0virtual-module',
-  //   plugins: [getExamplePlugin()],
-  //   output: [{
-  //     file: 'dist/virtual-module.js',
-  //     format: 'es'
-  //   }]
-  // }
+
