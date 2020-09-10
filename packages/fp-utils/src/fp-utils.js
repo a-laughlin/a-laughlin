d9d0d3b9c41c0f3b2c81ea5ca0ea87c4967509be
alaughlin <alaughlin@switchfly.com>
2020-09-10 15:07:56 -0600

    cleanup, removing executed functions to facilitate tree shaking

--- packages/fp-utils/src/fp-utils.js
+++ packages/fp-utils/src/fp-utils.js
@@ -1,57 +1,5 @@
-// curry/compose/pipe, for later fns
-let curry,compose,pipe;
-export const identity=x=>x;
-if (globalThis.process.env.NODE_ENV !== 'production') {
-  // debugging versions
-  const fToString = fn => fn.name ? fn.name : fn.toString();
-  curry =(fn) => {
-    const f1 = (...args) => {
-      if (args.length >= fn.length) { return fn(...args) };
-      const f2 = (...more) => f1(...args, ...more);
-      f2.toString = () => `${fToString(fn)}(${args.join(', ')})`;
-      return Object.defineProperty(f2, `name`, { value: `${fToString(fn)}(${args.join(', ')})` });
-    };
-    f1.toString = () => fToString(fn);
-    return Object.defineProperty(f1, `name`, { value: fToString(fn) });
-  };
-
-  // based on https://dev.to/ascorbic/creating-a-typed-compose-function-in-typescript-3-351i
-
-  compose = (...fns) => {
-    if (fns.length===0)return identity;
-    if (fns.length===1)return fns[0];
-    const fn=fns[fns.length-1];
-    fns=fns.slice(0,fns.length-1);
-    const composed = (...args) => fns.reduceRight((acc, f) => f(acc), fn(...args));
-    composed.toString = () => `compose(${fns.map(fToString).join(', ')})`;
-    return composed;
-  };
-  pipe = (fn=identity,...fns) => {
-    if (fns.length===0)return fn;
-    const piped = (...args) => fns.reduce((acc, f) => f(acc), fn(...args));
-    piped.toString = () => `pipe(${fns.reverse().map(fToString).join(', ')})`;
-    return piped;
-  };
-} else {
-  // eslint-disable-next-line
-  curry = fn => (...args) => args.length >= fn.length ? fn(...args) : curry(fn.bind(null, ...args));
-  // eslint-disable-next-line
-  compose = (...fns) => (...args) =>{
-    if (fns.length===0)return identity;
-    if (fns.length===1)return fns[0];
-    return fns.slice(0,fns.length-1).reduceRight((acc, f) => f(acc), fns[fns.length-1](...args));
-  }
-  // eslint-disable-next-line
-  pipe = (fn=identity,...fns) => (...args) => {
-    if (fns.length===0)return fn;
-    return fns.reduce((acc, f) => f(acc), fn(...args));
-  }
-}
-export { curry, compose, pipe };
-
-
 // stubs
-
+export const identity=x=>x;
 export const stubNull = ()=>null;
 export const stubArray = ()=>[];
 export const stubObject = ()=>({});
@@ -64,7 +12,6 @@ export const range=(end=10,start=0,step=1,mapper=identity)=>{
   while(start<end)result[result.length]=mapper(start+=step);
   return result;
 }
-
 
 
 // primitive predicates
@@ -80,29 +27,18 @@ export const is = val1=>val2=>val1===val2;
 export const isUndefOrNull = val => val === undefined || val === null;
 export const isProductionEnv = ()=>process.env.NODE_ENV === 'production';
 export const isPromise = x=>typeof x==='object'&&x!==null&&typeof x.then==='function';
-export const toPredicate = x=>{
-  if(isFunction(x)) return x;
-  if(isArray(x)) return matchesProperty(x);
-  if(isObjectLike(x)) return matches(x);
-  if(isString(x)) return hasKey(x)
-  if(stubTrue(x)) return stubFalse;
-};
+
 
 // debugging
 export const plog = (msg='')=>pipeVal=>console.log(msg,pipeVal) || pipeVal;
 
 // flow
 export const dpipe = (data,...args)=>pipe(...args)(data);
+
 // functions
-const makeCollectionFn=(arrayFn,objFn)=>(...args)=>ifElse(isArray,arrayFn(...args),objFn(...args));
-
-export const invokeArgsOnObj = (...args) => mapValues(fn=>fn(...args));
-export const invokeObjectWithArgs = (obj)=>(...args) => mapValues(fn=>isFunction(fn) ? fn(...args) : fn)(obj);
-
-export const overObj = (fnsObj={})=>(...args)=>mo(f=>f(...args))(fnsObj);
-export const overArray = (fnsArray=[])=>(...args)=>ma(f=>f(...args))(fnsArray);
+export const overObj = (fnsObj={})=>(...args)=>mapToObject(f=>f(...args))(fnsObj);
+export const overArray = (fnsArray=[])=>(...args)=>mapToArray(f=>f(...args))(fnsArray);
 export const over = x=>isArray(x)?overArray(x):overObj(x);
-export const converge = over;//backwards compat;
 
 // casting
 export const constant = x=>_=>x;
@@ -115,10 +51,7 @@ export const ensurePropIsArray = ensurePropWith(stubArray);
 export const ensurePropIsObject = ensurePropWith(stubObject);
 
 // logic
-// export const matches=o=>mo(_is)(o)(v,k)=>ma(v)k=>o=>k in o;
-// const matches=o=>and(ma((v,k)=>is)(o),(o)
 export const not = fn=>(...args)=>!fn(...args);
-export const ifElseUnary = (pred,T,F=identity)=>arg=>pred(arg)?T(arg):F(arg);
 export const ifElse = (pred,T,F=identity)=>(...args)=>(pred(...args) ? T : F)(...args);
 export const and = (...preds)=>(...args)=>{
   // console.log(`preds`,preds)
@@ -139,10 +72,16 @@ export const _is = (x) => (y) => x===y;
 export const hasKey=(k='')=>(coll={})=>k in coll;
 export const matchesProperty=([k,v]=[])=>(o={})=>o[k]===v;
 export const matches=(coll={})=>and(...mapToArray((v,k)=>matchesProperty([k,v]))(coll));
-export const none = compose(not,or);
+export const none = (...preds)=>not(or(...preds));
 export const condNoExec = (...arrs)=>(...x)=>{for (const [pred,val] of arrs) if(pred(...x)) return val;};
 export const cond = (...arrs)=>(...x)=>{for (const [pred, fn] of arrs) if (pred(...x)) return fn(...x);};
-
+export const toPredicate = x=>{
+  if(isFunction(x)) return x;
+  if(isArray(x)) return matchesProperty(x);
+  if(isObjectLike(x)) return matches(x);
+  if(isString(x)) return hasKey(x)
+  if(stubTrue(x)) return stubFalse;
+};
 
 
 // Array methods
@@ -196,78 +135,35 @@ export const immutableFilterObjectToObject = (pred=(v,k,c)=>true) => (coll={}) =
   return changed===true?acc:coll;
 }
 
-export const transToObject = makeCollectionFn(transArrayToObject,transObjectToObject);
-export const ro=transToObject; // backward compatability 
-export const transToArray = makeCollectionFn(transArrayToArray,transObjectToArray);
-export const ra=transToArray; // backward compatability
-export const transToSame = makeCollectionFn(transArrayToArray,transObjectToObject);
-export const rx = transToSame; // backward compatability
+export const transToObject = (...fn)=>ifElse(isArray,transArrayToObject(...fn),transObjectToObject(...fn));
+export const transToArray = (...fn)=>ifElse(isArray,transArrayToArray(...fn),transObjectToArray(...fn));
+export const transToSame = (...fn)=>ifElse(isArray,transArrayToArray(...fn),transObjectToObject(...fn));
 export const filterToArray =pred=>transToArray((a,v,k)=>pred(v,k)&&(a[a.length]=v)); // _ equiv filter
-export const fa=filterToArray; // backward compatability
 export const filterToObject=pred=>transToObject((a,v,k)=>pred(v,k)&&(a[k]=v)); // _ equiv pickBy
-export const fo=filterToObject; // backward compatability
-export const filterToSame=makeCollectionFn(filterToArray,filterToObject);
-export const fx=filterToSame;// backward compatability
+export const filterToSame=(...fn)=>ifElse(isArray,filterToArray(...fn),filterToObject(...fn));
 export const omitToArray=pred=>transToArray((a,v,k)=>!pred(v,k)&&(a[a.length]=v)); // _ equiv withoutBy
-export const oa=omitToArray; // backward compatability
 export const omitToObject=pred=>transToObject((a,v,k)=>!pred(v,k)&&(a[k]=v)); // _ equiv pickBy
-export const oo=omitToObject; // backward compatability
-export const omitToSame=makeCollectionFn(omitToArray,omitToObject);
-export const ox=omitToSame; // backward compatability
+export const omitToSame=(...fn)=>ifElse(isArray,omitToArray(...fn),omitToObject(...fn));
 export const mapToArray=fn=>transToArray((a,v,k)=>a[a.length]=fn(v,k)); // _ equiv map
-export const ma=mapToArray; // backward compatability
 export const mapToObject=fn=>transToObject((a,v,k)=>a[k]=fn(v,k)); // _ equiv mapValues
-export const mo=mapToObject; // backward compatability
-export const mapToSame=makeCollectionFn(mapToArray,mapToObject);
-export const mx=mapToSame; // backward compatability
+export const mapToSame=(...fn)=>ifElse(isArray,mapToArray(...fn),mapToObject(...fn));
 export const filterMapToArray = (pred,mapper)=>transToArray((a,v,k)=>pred(v,k)&&(a[a.length]=mapper(v,k)));
-export const fma = filterMapToArray; // backward compatability
 export const filterMapToObject = (pred,mapper)=>transToObject((o,v,k)=>pred(v,k)&&(o[k]=mapper(v,k)));
-export const fmo = filterMapToObject; // backward compatability
-export const filterMapToSame=makeCollectionFn(filterMapToArray,filterMapToObject);
-export const fmx=filterMapToSame; // backward compatability
+export const filterMapToSame=(...fn)=>ifElse(isArray,filterMapToArray(...fn),filterMapToObject(...fn));
 
-export const first = c=>{
-  if (isArray(c)) return c[0];
-  for (const k in c)return c[k];
-}
-export const last = c =>{
-  c=isArray(c)?c:Object.values(c);
-  return c[c.length-1];
-}
-const partitionObject = (...preds)=>overArray([...(preds.map(p=>fo(p))),fo(none(...preds))]);
-const partitionArray = (...preds)=>overArray([...(preds.map(fa)),fa(none(...preds))]);
-export const partition = makeCollectionFn(partitionArray,partitionObject);
+export const first = c=>(isArray(c)?c:Object.values(c))[0];
+export const last = c =>(isArray(c)??(c=Object.values(c)))[c.length-1];
+const partitionObject = (...preds)=>overArray([...(preds.map(p=>filterToObject(p))),filterToObject(none(...preds))]);
+const partitionArray = (...preds)=>overArray([...(preds.map(filterToArray)),filterToArray(none(...preds))]);
+export const partition = (...preds)=>ifElse(isArray,partitionArray(...preds),partitionObject(...preds));
 
 
 // collection predicates
-export const len = makeCollectionFn(
-  num=>({length})=>length===num,
-  num=>coll=>{
-    let l=0,k;
-    for (k in coll)if(++l>num)return false;
-    return l===num;
-  }
-);
-export const len0 = len(0);
-export const len1 = len(1);
+export const isLength = num=>x=>num===((isArray(x)&&x.length) ?? (isObjectLike(x) && Object.keys(x).length) ?? (isFunction(x) && x.length) ?? 0);
 export const has = key=>obj=>key in obj;
-export const isDeepEqual=(a,b)=>{ // decently fast check on objects and arrays
-  if (a===b)return true;
-  if (typeof a !== 'object')return a===b;
-  if (Array.isArray(a)){
-    if (Array.isArray(b)===false||a.length!==b.length)return false;
-    let key=-1,L=toCheck.length;
-    while (++key<L) if (isCached(a[key],b[key])===false) return false;
-    return true;
-  }
-  let key;
-  for (key in toCheck) if (isCached(a[key],b[key])===false) return false;
-  return true;
-}
 
 // indexers
-export const keyBy = ifElseUnary(isString,
+export const keyBy = ifElse(isString,
   (id='id')=>transToObject((o,v,k)=>{o[v[id]]=v;}),
   (fn=x=>x.id)=>transToObject((o,v,k)=>{o[fn(v,k)]=v;})
 );
@@ -290,7 +186,7 @@ export const pget = cond( // polymorphic get
     return targ=>str.reduce((t,s)=>isArray(t) && !isInteger(+s) ? t.map(o=>o[s]) : t[s], targ)
   }],
   [isArray,keys=>pick(keys)],
-  [isObjectLike, obj=>target=>mo(f=>pget(f)(target))(obj)],
+  [isObjectLike, obj=>target=>mapToObject(f=>pget(f)(target))(obj)],
   [stubTrue,identity], // handles the function case
 );
 export const pick=cond(
@@ -300,40 +196,15 @@ export const pick=cond(
   [stubTrue,keys=>obj=>new Error('unsupported type for pick: '+typeof keys)]
 );
 
-
-
-
-// Objects
-
-export const objStringifierFactory = ({
-  getPrefix=()=>'',
-  getSuffix=()=>'',
-  mapPairs=x=>x,
-  keySplit = '=',
-  pairSplit = '&'
-} = {})=>(input={})=>{
-  const output = Object.keys(input)
-  .map(k=>([k, input[k]].map(mapPairs).join(keySplit)))
-  .join(pairSplit);
-  return getPrefix(input,output) + output + getSuffix(input, output);
-};
-export const objToUrlParams = objStringifierFactory({
-  getPrefix:(input,output)=>output ? '?' : '',
-  mapPairs:encodeURIComponent,
-});
-
 // content
-export const uniqueId = (()=>{
-  let id=0;
-  return (predix='')=>`${predix}${++id}`;
-})();
+export const uniqueId = (start=0,fn=identity)=>(...args)=>fn(start++,...args);
 
 
 
 
 
 
-
+// transducers
 export const transduce = (acc, itemCombiner , transducer, collection) =>
   tdReduceListValue(transducer(itemCombiner))(acc,collection);
 
@@ -462,6 +333,7 @@ export const diffObjs = (a={},b={}) => {
 export const diffBy = (by=x=>x.id, args = []) => by ? diffObjs(...args.map(keyBy(by))) : diffObjs(args);
 
 
+
 // export const diffBy = (by, reducer) => (args = []) => {
 //   const diff = by ? diffObjs(args.map(keyBy(by))) : diffObjs(args);
 //   const { anb, anbc, bna, bnac, aib, aibc, aub, aubc, changed, changedc, a, b } = diff;
@@ -505,3 +377,53 @@ export const diffBy = (by=x=>x.id, args = []) => by ? diffObjs(...args.map(keyBy
 //     return reused;
 //   }
 // };
+
+
+let curry,compose,pipe;
+if (globalThis.process.env.NODE_ENV !== 'production') {
+  // debugging versions
+  const fToString = fn => fn.name ? fn.name : fn.toString();
+  curry =(fn) => {
+    const f1 = (...args) => {
+      if (args.length >= fn.length) { return fn(...args) };
+      const f2 = (...more) => f1(...args, ...more);
+      f2.toString = () => `${fToString(fn)}(${args.join(', ')})`;
+      return Object.defineProperty(f2, `name`, { value: `${fToString(fn)}(${args.join(', ')})` });
+    };
+    f1.toString = () => fToString(fn);
+    return Object.defineProperty(f1, `name`, { value: fToString(fn) });
+  };
+
+  // based on https://dev.to/ascorbic/creating-a-typed-compose-function-in-typescript-3-351i
+
+  compose = (...fns) => {
+    if (fns.length===0)return identity;
+    if (fns.length===1)return fns[0];
+    const fn=fns[fns.length-1];
+    fns=fns.slice(0,fns.length-1);
+    const composed = (...args) => fns.reduceRight((acc, f) => f(acc), fn(...args));
+    composed.toString = () => `compose(${fns.map(fToString).join(', ')})`;
+    return composed;
+  };
+  pipe = (fn=identity,...fns) => {
+    if (fns.length===0)return fn;
+    const piped = (...args) => fns.reduce((acc, f) => f(acc), fn(...args));
+    piped.toString = () => `pipe(${fns.reverse().map(fToString).join(', ')})`;
+    return piped;
+  };
+} else {
+  // eslint-disable-next-line
+  curry = fn => (...args) => args.length >= fn.length ? fn(...args) : curry(fn.bind(null, ...args));
+  // eslint-disable-next-line
+  compose = (...fns) => (...args) =>{
+    if (fns.length===0)return identity;
+    if (fns.length===1)return fns[0];
+    return fns.slice(0,fns.length-1).reduceRight((acc, f) => f(acc), fns[fns.length-1](...args));
+  }
+  // eslint-disable-next-line
+  pipe = (fn=identity,...fns) => (...args) => {
+    if (fns.length===0)return fn;
+    return fns.reduce((acc, f) => f(acc), fn(...args));
+  }
+}
+export { curry, compose, pipe };
