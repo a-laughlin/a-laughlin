95307cdd84275a5ed466c1e2e20e501ec59e2dc5
alaughlin <alaughlin@switchfly.com>
2020-10-01 13:58:59 -0600

    solid refactor - tests still breaking, but simpler to think through

--- packages/gqdux/src/schemaToQuerySelector.js
+++ packages/gqdux/src/schemaToQuerySelector.js
@@ -1,6 +1,6 @@
-import {transToObject,identity,indexBy, appendArrayReducer, appendObjectReducer, setImmutableNonEnumProp, mapToObject, compose, tdToSame, transToSame} from '@a-laughlin/fp-utils';
+import {transToObject,identity,indexBy, appendArrayReducer, appendObjectReducer, setImmutableNonEnumProp, mapToObject, compose, tdToSame, transToSame, over, mapToArray, tdMap, mapToSame} from '@a-laughlin/fp-utils';
 import indexSchema from './indexSchema';
-import {filter,omit,intersect} from './transducers';
+import {filter,omit,implicit} from './transducers';
 
 // iteration sets properties and checks changes.  After iteration choose which parent to return, so unchanged properties result in an unchanged parent.
 const tdReduceEachWithChanged=(reduceChild,preTrans=identity,childKeys=(p,[,pN={}],k)=>Object.keys(pN))=>nextReducer=>preTrans((parent,arr,k)=>{
@@ -11,61 +11,6 @@ const tdReduceEachWithChanged=(reduceChild,preTrans=identity,childKeys=(p,[,pN={
   }
   return nextReducer(nextParent,arr,k,changed);
 });
-
-const getTraversalMapper = (
-  chooseParent=(p,[pP,pN,pNP],k,changed)=>changed||pN!==pNP?p:pP,
-  objectListCombiner=appendObjectReducer,
-  arrayListCombiner=appendArrayReducer
-)=>{
-  // 5 descend and iterate cases, 1 leaf (scalar value)
-  // take the parent arr, and use v/k to map the parentArray's sections for the next level
-  const reduce=tdReduceEachWithChanged;
-  const reducers={};
-  reducers.object            = reduce(
-    (p={},[pP,pN,pNP,rN,rNP,pqi],k)=>{
-      if (!(k in pqi)) return p;
-      // if(pqi[k].meta.defKind==='scalar'){
-      //   console.log(`pqi[k].meta.fieldName`,k,JSON.stringify(pqi.args),JSON.stringify(pqi[k].args),pqi[k].meta.fieldName);
-      //   return pqi[k].transducer(objectListCombiner)(p,[pP,pN,pNP,rN,rNP,pqi],k);
-      //   // return objectListCombiner(p,k in pqi.args?pqi.args[k]:pN[k],k);
-      // }
-      if(pqi[k].nodeType==='objectScalar') return objectListCombiner(p,pN[k],k);
-      if(pqi[k].nodeType==='objectScalarList') return objectListCombiner(p,pN[k],k);
-      if(pqi[k].nodeType==='objectId') {
-        console.log(pN)
-        return objectListCombiner(p,reducers.object(p[k],[pP?.[k],rN[pqi[k].meta.defName][pN[k]],rNP?.[pqi[k].meta.defName]?.[pN[k]],rN,rNP,pqi[k]],pN[k]),k);
-      }
-      if(pqi[k].nodeType==='objectId'){
-        console.log(pN)
-        return objectListCombiner(p,reducers.object(p[k],[pP?.[k],rN[pqi[k].meta.defName][pN[k]],rNP?.[pqi[k].meta.defName]?.[pN[k]],rN,rNP,pqi[k]],pN[k]),k);
-      }
-      if(pqi[k].nodeType==='objectIdList') {
-        console.log(pN)
-        return objectListCombiner(p,[],k);
-      }
-      // transducer for each type
-      // need to switch collections?
-      return objectListCombiner(p,transToObject((l,v,id)=>{
-        // pqi[k].transducer(objectListCombiner)(l,[pP?.[k]?.[id],v,pNP?.[k]?.[id],rN,rNP,pqi[k]],id);
-        return reducers.object(l,[pP?.[k]?.[id],v,pNP?.[k]?.[id],rN,rNP,pqi[k]],id);
-        // return pqi[k].transducer(objectListCombiner)(l,[pP?.[k]?.[id],v,pNP?.[k]?.[id],rN,rNP,pqi[k]],id);
-      })(pN[k]),k);
-      // return objectListCombiner(p,
-      //   reducers[pqi[k].meta.nodeType](p[k],[pP?.[k],pN[k],pNP?.[k],rN,rNP,pqi[k]],pN[pqi[k].meta.fieldName]),
-      // k);
-    },
-    identity,
-    (p,[,,,,,pqi])=>Object.keys(pqi)
-  )(chooseParent);
-  // reducers.objectObjectList  = reduce((p={},[pP,pN={},pNP={},rN,rNP,pqi],k)=>objectListCombiner(p,reducers.object({},[pP?.[k],pN?.[k],pNP?.[k],rN,rNP,pqi],k),k))(chooseParent);
-  // reducers.objectId          = (p,[pP,pN,,rN,rNP,pqi],k)=>reducers.object({},[pP,rN[pqi[k].meta.defName][pN],rNP?.[pqi[k].meta.defName]?.[pN],rN,rNP,pqi[k]]);
-  // reducers.objectIdList      = reduce((p=[],[pP,pN,,rN,rNP,pqi],k)=>arrayListCombiner(p,
-  //   reducers.object({},[pP?.[k],rN[pqi.meta.defName][pN[k]],rNP[pqi.meta.defName]?.[pN[k]],rN,rNP,pqi[k]],""),k)
-  // )(chooseParent);
-  // reducers.objectScalarList      = (p,[,pN],k)=>pN;
-  // reducers.objectScalar      = (p,[,pN],k)=>pN;
-  return reducers;
-}
 
 // returns a function that populates query arguments with passed variables
 const getArgsPopulator = vars=>{
@@ -85,40 +30,61 @@ const variableDefinitionsToObject = (variableDefinitions=[],passedVariables={})=
 
 const getNodeType = ({isList,defKind,defName,fieldName,fieldKindName})=>{
   if (defKind==='scalar') return  (isList ? 'objectScalarList' : 'objectScalar');
-  if (isList) return (defName===fieldName ? 'objectObjectList' : 'objectIdList') ;
-  return (fieldKindName==='ID' ? 'objectId' : 'object');
+  if (defName===fieldName) return 'objectObjectList';
+  return isList ? 'objectId' : 'objectIdList';
 }
+
 const indexQuery=(schema={},query={},passedVariables={},transducers={})=>{
   let meta=indexSchema(schema).selectionMeta;
-  const tds={...transducers,implicit:intersect,filter:intersect};
-  // meta=meta._query;
   const getArgs = getArgsPopulator(variableDefinitionsToObject(query.definitions[0].variableDefinitions||[],passedVariables));
-  const inner=(parentNode={},s,meta)=>{
-    for (const ss of s.selectionSet.selections){
-      const node = parentNode[ss.name.value]={};
-      setImmutableNonEnumProp(node,'meta',meta[ss.name.value]);
-      setImmutableNonEnumProp(node.meta,'nodeType',getNodeType(node.meta));
-      setImmutableNonEnumProp(node,'args',{implicit:{}});
-      const args=getArgs(ss.arguments),trans={};
-      for (const arg in args){
-        (arg in tds?node.args:node.args.implicit)[arg]=args[arg];
-        const tkey=arg in tds?arg:'implicit';
-        trans[tkey]||(trans[tkey]=tds[tkey]);
+  const inner=(s,meta)=>{
+    const name=s.name?.value||meta.defName; // || root _query case
+    const nodeType = getNodeType(meta);
+    const childSelectors = transToObject((o,ss)=>o[ss.name.value]=inner(ss,meta[ss.name.value]));
+    const {explicit,implicit}=indexBy((v,k)=>k in transducers?'explicit':'implicit',(v,k)=>k)(getArgs(s.arguments));
+    const implicitArgsTransducer=implicit?transducers.implicit(implicit):identity;
+    const explicitArgsTransducer=explicit?compose(...Object.entries(explicit).map(([k,v])=>transducers[k](explicit[k]))):identity;
+    const childrenTransducer = nextReducer=>(undefinedValue,[vP,vN,vNP,rN,rNP])=>{
+      // descend then ascend the state path
+      let v;
+      if      (nodeType==='objectScalar') v = nextReducer(vN,[vP,vN,vNP,rN,rNP]);
+      else if (nodeType==='objectScalarList') v = nextReducer(vN,[vP,vN,vNP,rN,rNP]);
+      else if (nodeType==='objectObjectList') v = mapToSame((obj,id)=>{
+          return mapToSame((childValueReducer,ck)=>{
+            return childValueReducer([vP?.[id]?.[ck],vN?.[id]?.[ck],vP?.[id]?.[ck],rN,rNP]);
+          })(childSelectors);
+        })(vN);
+      else {
+        const relationMeta=meta[name];
+        const relName=relationMeta.defName;
+        const [relcN,relcNP] = [rN[relName],rNP[relName]];
+        let id=vN;
+        if (nodeType==='objectId') v = mapToSame((childValueReducer,ck)=>{
+            return childValueReducer([vP?.[ck],relcN?.[id]?.[ck],relcNP?.[id]?.[ck],rN,rNP]);
+          })(childSelectors)
+        else if (nodeType==='objectIdList') v = mapToSame(id=>{
+          return mapToSame((childValueReducer,ck)=>{
+            return childValueReducer([vP?.[ck],relcN?.[id]?.[ck],relcNP?.[id]?.[ck],rN,rNP]);
+          })(childSelectors);
+        })(vN);
       }
-      setImmutableNonEnumProp(node,'trans',trans);
-      setImmutableNonEnumProp(node,'transducer',ss.arguments.length===0?identity:compose(...Object.values(trans)));
-    }
-    return parentNode;
+      const changed=true;
+      return nextReducer(v,[vP,vN,vNP,rN,rNP],changed);
+    };
+    const reducer =  compose(
+      implicitArgsTransducer,
+      explicitArgsTransducer,
+      childrenTransducer,
+      nextReducer=>(v,[vP,vN,vNP,rN,rNP],changed)=>nextReducer(vN===vNP||changed ? v:vP,[vP,vN,vNP,rN,rNP],name)
+    )(appendObjectReducer);
+    return ([vP,vN,vNP,rN,rNP])=>reducer(undefined,[vP,vN,vNP,rN,rNP],name);
   }
-  const result=inner({},query.definitions[0],meta._query);
-  return result;
+  return inner({},query.definitions[0],meta._query);
 };
-// [pP={},pN={},pNP={},rN,rNP,pqi]
 export const schemaToQuerySelector=(schema,transducers={})=>(query,passedVariables)=>{
-  const queryIndex=indexQuery(schema,query,passedVariables,transducers);
-  const mapObject=getTraversalMapper().object;
+  const mapQuery=indexQuery(schema,query,passedVariables,{...transducers,implicit});
   return (rootNorm={},rootNormPrev={},rootDenormPrev={})=>{
-    return mapObject({},[rootDenormPrev,rootNorm,rootNormPrev,rootNorm,rootNormPrev,queryIndex]);
+    return mapQuery([rootDenormPrev,rootNorm,rootNormPrev,rootNorm,rootNormPrev]);
   };
 }
 export const schemaToMutationReducer=(schema,transducers={})=>{
--- packages/gqdux/src/transducers.js
+++ packages/gqdux/src/transducers.js
@@ -1,7 +1,26 @@
-import {diffBy, toPredicate} from "@a-laughlin/fp-utils"
+import {diffBy, tdFilter, toPredicate} from "@a-laughlin/fp-utils"
 
 export const filter=combiner=>(p,[pP,pN,pNP,rN,rNP,pqi],k)=>toPredicate(pqi.args)(pN,k)?combiner(p,[pP,pN,pNP,rN,rNP,pqi],k):p;
-export const intersect=filter;
+// export const intersect=combiner=>(p,[pP,pN,pNP,rN,rNP,pqi],k)=>{
+//   if (pqi.nodeType==='objectScalar'){}
+//   if (pqi.nodeType==='objectScalarList'){}
+//   if (pqi.nodeType==='objectId'){}
+//   if (pqi.nodeType==='objectIdList'){}
+//   if (pqi.nodeType==='objectObjectList'){}
+//   if (pqi.nodeType==='object'){}
+// };
+// export const toPredicate = x=>([v,vP,vN,vNP,rN,rNP,pqi],k)=>{
+//   if(isFunction(x)) return x(vN,k);
+//   if(isArray(x)) return matchesProperty(x);
+//   if(isObjectLike(x)) return matches(x);
+//   if(isString(x)) return hasKey(x)
+//   if(stubTrue(x)) return stubFalse;
+// };
+export const implicit=args=>tdFilter((v,[vP,vN],k)=>{
+  // we'll need to sub variables and objects before hitting this
+  if (!(k in args)) return true;
+  return args[k]===vN[k];
+});
 export const omit=combiner=>(acc,arr,id,arg)=>!(toPredicate(arg)(arr[3],id))?combiner(acc,arr,id):acc;
 export const identity=mapSelection=>mapSelection;
 export const subtract=omit;
